15e3bca12a96a19419c5737a16a04f30b3da4404
Fares Ahmed Moustafa <F.Ahmed2270@nu.edu.eg>
2024-11-16 23:21:07 +0200

    Test_ADD.py

--- Test_ADD.py
+++ Test_ADD.py
@@ -1,7 +1,7 @@
 import unittest
-from addition import add_numbers
+from ADD import add_numbers
 
-class TestAdditionFunction(unittest.TestCase):
+class Test_ADD(unittest.TestCase):
     def test_add_positive_numbers(self):
         self.assertEqual(add_numbers(2, 3), 5)
 
@@ -11,5 +11,3 @@ class TestAdditionFunction(unittest.TestCase):
     def test_add_mixed_numbers(self):
         self.assertEqual(add_numbers(-2, 3), 1)
 
-if __name__ == "__main__":
-    unittest.main()
